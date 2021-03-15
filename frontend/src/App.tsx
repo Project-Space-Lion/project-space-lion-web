e60a1f4144f3151c0f806fbadf046880d2cee7ae
sbuule48 <sbuule48@gmail.com>
2021-03-15 19:09:30 -0400

    added router to apt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import Product from './Products/Products';
 import Home from './Home/Home';
 import data from './Products/data';
+import Products from './Products/Products';
 
 function App() {
    const openMenu = () => {
@@ -14,90 +15,62 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
   return (
-      <div className="grid-container">
+    <Router>
+    <div className="grid-container">
         <head>
-          <link rel="stylesheet" href="style.css"></link>
+            <link rel="stylesheet" href="style.css"></link>
             <title>Space Lion</title>
-          </head>
-          <body>
-                <header className="header">
-                    <div className="brand">
-                        <button onClick={openMenu}>&#9776;</button>
-                        <a href="index.html">Space Lion</a>
-                    </div>
-                    <div className="header-links">
-                        <a href="cart.html">Cart</a>
-                        <a href="signin.html">Sign In</a>
-                    </div>
-                </header>
-                <aside className="sidebar">
-                        <h3>Shopping Categories</h3>
-                        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                        <ul>
-                            <li>
-                                <a href="index.html">Pants</a>
-                            </li>
-
-                            <li>
-                                <a href="index.html">Shirts</a>
-                            </li>
-                        </ul>
-                </aside>
-                <main className="main">
-                    <div className="content">
-                        <ul className="products">
-                            <li>
-                                <div className="produtcs">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="produtcs">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="produtcs">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="produtcs">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </main>
-                <footer className="footer">
+        </head>
+    
+    
+    
+        <body>
+            <header className="header">
+                <div className="brand">
+                  
+                    <button onClick={openMenu}>&#9776;</button>
+                    <a href="index.html">Space Lion</a>
+                </div>
+                <div className="header-links">
+                   <Link to="/">Home </Link>
+                   <Link to="/catelog">Catelog</Link>
+                </div>
+            </header>
+    
+            <div>
+             <header></header>
+             <aside className="sidebar">
+                 <h3>Shopping categories</h3>
+                 <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                 <ul>
+                     <li>
+                         <a href="index.html">Pants</a>
+                     </li>
+                     <li>
+                         <a href="index.html">Shirts</a>
+                     </li>
+                 </ul>
+             </aside>
+             <main className="main">
+                 <Switch>
+                     <Route exact path="/">
+                         <Home />
+                     </Route>
+                     <Route path="/catalog">
+                         <Products />
+                     </Route>     
+                </Switch>            
+             </main>
+             <footer className="footer">
                     &copy; 2021 Space Lion
-                </footer>
-          </body>
-      </div>
-  );
-}
-export default App;
+    
+             </footer>
+            </div>
+        </body>
+    </div>
+    </Router>
+      );
+    }
+    
+    export default App;
+    
