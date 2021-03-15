af1db878d34fe7f856100bdef75d6e0f2b22bf0e
matthiga <matt28473@gmail.com>
2021-03-15 18:44:44 -0400

    app.tsx update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,25 +13,92 @@ function App() {
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
- <div className="content">
-    <ul className="products">
-    {
-        data.products.map(product =>
-            <li>
-                <div className = "product">
-                    <img className="product-image" src={product.imageUrl} alt="product" />
-                    <div className="product-name">
-                        <a href="product.html">{product.name}</a>
+  return
+  {
+      <div className="grid-container">
+        <head>
+          <link rel="stylesheet" href="style.css"></link>
+            <title>Space Lion</title>
+          </head>
+          <body>
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>&#9776;</button>
+                        <a href="index.html">Space Lion</a>
                     </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">{product.price}</div>
-                    <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
-                </div>
-            </li>)
-    }
-    </ul>
- </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign In</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                        <h3>Shopping Categories</h3>
+                        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                        <ul>
+                            <li>
+                                <a href="index.html">Pants</a>
+                            </li>
 
-        }
-
+                            <li>
+                                <a href="index.html">Shirts</a>
+                            </li>
+                        </ul>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <ul className="products">
+                            <li>
+                                <div className="produtcs">
+                                    <img className="product-image" src="image/d1.jpg" alt="product" />
+                                    <div className="product-name">
+                                        <a href="product.html">Slim Shirt</a>
+                                    </div>
+                                    <div className="product-brand">Nike</div>
+                                    <div className="product-price">$60</div>
+                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="produtcs">
+                                    <img className="product-image" src="image/d1.jpg" alt="product" />
+                                    <div className="product-name">
+                                        <a href="product.html">Slim Shirt</a>
+                                    </div>
+                                    <div className="product-brand">Nike</div>
+                                    <div className="product-price">$60</div>
+                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="produtcs">
+                                    <img className="product-image" src="image/d1.jpg" alt="product" />
+                                    <div className="product-name">
+                                        <a href="product.html">Slim Shirt</a>
+                                    </div>
+                                    <div className="product-brand">Nike</div>
+                                    <div className="product-price">$60</div>
+                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="produtcs">
+                                    <img className="product-image" src="image/d1.jpg" alt="product" />
+                                    <div className="product-name">
+                                        <a href="product.html">Slim Shirt</a>
+                                    </div>
+                                    <div className="product-brand">Nike</div>
+                                    <div className="product-price">$60</div>
+                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </main>
+                <footer className="footer">
+                    &copy; 2021 Space Lion
+                </footer>
+          </body>
+      </div>
+  }
+}
 export default App;
